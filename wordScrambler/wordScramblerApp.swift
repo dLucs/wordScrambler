4e211b88c9050f901bb8754a61b28e798d60da94
dLucs <lucas_61@hotmail.es>
2024-01-18 11:49:20 +0100

    Initial Commit

---
+++ wordScrambler/wordScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScramblerApp.swift
+//  wordScrambler
+//
+//  Created by Lucas on 18/1/24.
+//
+
+import SwiftUI
+
+@main
+struct wordScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
